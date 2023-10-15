45da7adc5469c38f130132081e16458a2089e984
Mohamed Abdallah <mohaabdallah2002287@gmail.com>
2023-10-15 22:41:44 +0300

    end test

--- app/(tabs)/MyAds.js
+++ app/(tabs)/MyAds.js
@@ -1,14 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { View, Text } from "../../components/Themed";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Image, useColorScheme, View as ReactView } from "react-native";
+import { Image, useColorScheme, View as ReactView, TouchableOpacity, Text as ReactText } from "react-native";
 import WordsContext from "../../src/lang/wordsContext";
 import directionContext from "../../src/direction/directionContext";
-import { TouchableOpacity } from "react-native-gesture-handler";
+
 
 
 export default function MyAds() {
     const colorScheme = useColorScheme();
+    const [load, setLoad] = useState(false);
     const Lungaues = useContext(WordsContext);
     const direction = useContext(directionContext);
     function ImageLogo() {
@@ -31,16 +32,16 @@ export default function MyAds() {
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: (colorScheme == 'dark') ? 'black' : 'white' }}>
             <View style={{ flex: 1, alignItems: 'center' }}>
-                <View style={{ flexDirection: "row", width: "100%", justifyContent: "space-evenly" , alignItems: "center" }}>
+                <View style={{ flexDirection: "row", width: "100%", justifyContent: "space-evenly", alignItems: "center" }}>
                     <ImageLogo />
                 </View>
                 <View style={{ width: "100%", flexDirection: 'row' }}>
                     <View style={{ width: "100%", alignItems: 'center', justifyContent: 'center' }}>
-                        <TouchableOpacity style={{ width: "100%", height: 50 }}>
+                        <ReactView style={{ height: 50 }}>
                             <Text style={{ width: "100%", fontSize: 15, fontWeight: "bold" }}>
                                 My Ads
                             </Text>
-                        </TouchableOpacity>
+                        </ReactView>
                     </View>
                     {/* <View style={{ width: "50%", alignItems: 'center', justifyContent: 'center' }}>
                         <TouchableOpacity style={{ width: "100%", height: 50 }}>
@@ -50,30 +51,55 @@ export default function MyAds() {
                         </TouchableOpacity>
                     </View> */}
                 </View>
-                <View style={{ width: "90%" }}>
-                    <View style={{ width: "100%", height: 40, backgroundColor: 'gray', flexDirection: "row", alignItems: "center", justifyContent: 'space-around' }}>
+                <View style={{ width: "90%", alignItems: "center" }}>
+                    <View style={{ width: "100%", height: 40, backgroundColor: 'gray', flexDirection: direction.direction, alignItems: "center", justifyContent: 'space-around' }}>
                         <ReactView style={{ width: "80%" }}>
                             <Text>
                                 From: 28 Oct 22
                             </Text>
                         </ReactView>
-                        <ReactView style={{ width: "10%" }}>
-                            <Text>
-                                DO
-                            </Text>
-                        </ReactView>
+                        <TouchableOpacity style={{ width: "10%" }} onPress={() => {
+                            setLoad(true);
+                        }}>
+                            <ReactView style={{ width: 4, height: 4, borderRadius: 50, backgroundColor: 'black', marginBottom: 2 }} />
+                            <ReactView style={{ width: 4, height: 4, borderRadius: 50, backgroundColor: 'black', marginBottom: 2 }} />
+                            <ReactView style={{ width: 4, height: 4, borderRadius: 50, backgroundColor: 'black', marginBottom: 2 }} />
+                        </TouchableOpacity>
                     </View>
-                    <View>
-                        <View style={{}}>
+                    <View style={{ width: "100%", flexDirection: direction.direction, justifyContent: 'space-between' }} >
+                        <View style={{ width: "52%" }}>
                             <Image source={require('../../src/assets/house1.jpeg')} />
                         </View>
-                        <View style={{}}>
-                            <Text>
-                                some Data
+                        <View style={{ width: "47%", justifyContent: 'space-between' }}>
+                            <Text style={{ fontSize: 15, fontWeight: 'bold' }} numberOfLines={3}>
+                                some Datasome Datasome Datasome Datasome Datasome Datasome Datasome Datasome Datasome Data
+                            </Text>
+                            <Text style={{ fontSize: 15, fontWeight: 'bold' }} numberOfLines={2}>
+                                some Datasome Datasome Datasome Datasome Datasome Datasome Datasome Datasome Datasome Datasome Data
+                            </Text>
+                            <Text style={{ fontSize: 15, fontWeight: 'bold' }} numberOfLines={1}>
+                                some Datasome Datasome Datasome Datasome Datasome Datasome Datasome Datasome Datasome Datasome Data
                             </Text>
                         </View>
                     </View>
                 </View>
+                {
+                    load ? (
+                        <>
+                            <ReactView style={{ height: "100%", width: "100%", top: 0, bottom: 0, position: 'absolute', backgroundColor: 'gray', opacity: 0.5, alignItems: "center", justifyContent: "center" }}>
+                            </ReactView>
+                            <ReactView style={{ width: "90%", height: 200, backgroundColor: "white", opacity: 1 }}>
+                                <TouchableOpacity onPress={() => {
+                                    setLoad(false);
+                                }}>
+                                    <ReactText>
+                                        Delete It
+                                    </ReactText>
+                                </TouchableOpacity>
+                            </ReactView>
+                        </>
+                    ) : (<></>)
+                }
             </View>
         </SafeAreaView>
     )
