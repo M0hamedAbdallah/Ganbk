064859950efd5799fc385fc08a4597f00e814822
Mohamed Abdallah <mohaabdallah2002287@gmail.com>
2023-10-24 02:05:07 +0300

    Update MyAds.js

--- app/(tabs)/MyAds.js
+++ app/(tabs)/MyAds.js
@@ -5,18 +5,21 @@ import { Image, useColorScheme, View as ReactView, TouchableOpacity, Text as Rea
 import WordsContext from "../../src/lang/wordsContext";
 import directionContext from "../../src/direction/directionContext";
 import auth, { db } from "../../firebase/config/firebase-config";
-import { doc, getDoc } from "firebase/firestore";
+import { deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
 import { firebase } from "../../firebase/config/firebase-config";
+import { getStorage, ref, deleteObject } from "firebase/storage";
 import Modal from "react-native-modal";
 
 
 export default function MyAds() {
+    const storage = getStorage();
     const colorScheme = useColorScheme();
-    const [load, setLoad] = useState(false);
     const [load2, setLoad2] = useState(false);
     const [MyAds, setMyAds] = useState([]);
-    const [MyAdsVersion, setMyAdsVersion] = useState([]);
     const [MyAdsDetails, setMyAdsDetails] = useState([]);
+    const [valueData, setvalueData] = useState([]);
+    const [deleteIt, setDeleteIt] = useState({});
+    const [indexItem, setindexItem] = useState(-1);
     const [value, setValue] = useState(0);
     const [plus, setPlus] = useState(0);
     const [plus1, setPlus1] = useState(0);
@@ -46,20 +49,16 @@ export default function MyAds() {
 
 
     useEffect(() => {
+        setvalueData([]);
         const login = async () => {
             const UserRef = doc(db, "Users", auth?.currentUser?.uid);
             const docSnap = await getDoc(UserRef);
             const data = docSnap.data();
             setMyAds((data.MyAds));
-            // setMyAdsDetails([]);
-            return Math.round() * 1000;
         }
         if (auth?.currentUser) {
-            setPlus(
-                login()
-            )
+            login()
         }
-        return login;
     }, [value])
 
 
@@ -68,22 +67,85 @@ export default function MyAds() {
         const login = () => {
             MyAds?.map(async ({ from, num }) => {
                 firebase.firestore().doc('/' + from + '/' + num).onSnapshot((value) => {
-                    MyAdsDetails.push(value.data());
-                    // console.log(value.data())
-
+                    if (value.data() != undefined) {
+                        valueData.push(value.data());
+                    }
                 })
             })
-            return Math.round() * 1000;
+            setMyAdsDetails(valueData);
         }
-        // if (MyAdsDetails.length == 0) {
-        setPlus1(
-            login()
-        );
-        // }
-        console.log(MyAdsDetails)
-        return login;
-    }, [])
+        login();
+    }, [value])
 
+    const DeleteTheAds = async (dataSended) => {
+        let sw = false;
+        try {
+            const Ad = (dataSended?.Photo)?.map(async (ImageUri) => {
+                const Sended = await new Promise((resolve, reject) => {
+                    const Arrayone = ImageUri.split("/");
+                    const Arraytwo = Arrayone[(Arrayone?.length) - 1].split("?");
+                    const Arraythree = Arraytwo[0].replace("%", "/");
+                    const Uri = Arraythree + ".jpeg";
+                    const desertRef = ref(storage, Uri);
+                    deleteObject(desertRef).then(() => {
+                        // File deleted successfully
+                        sw = true;
+                    }).catch((error) => {
+                        // Uh-oh, an error occurred!
+                        sw = false;
+                        console.log(error)
+                        // throw error(0);
+                    });
+                    console.log(Uri);
+                    sw = true;
+                })
+            })
+            Promise.all(Ad);
+            if (sw) {
+                const theAdsD = MyAds[indexItem];
+                const AdInFire = await new Promise(async (resolve, reject) => {
+                    const UserRef = doc(db, theAdsD?.from, "0");
+                    const docSnap = await getDoc(UserRef);
+                    if (docSnap.data() != undefined) {
+                        const num = parseInt(docSnap.data().number);
+                        const array = docSnap.data().DataList;
+                        const arrayDeleted = docSnap.data().Deleted;
+                        const UserRef1 = doc(db, theAdsD?.from, theAdsD?.num);
+                        arrayDeleted.push(theAdsD?.num);
+                        arrayDeleted.sort((a, b) => a - b);
+                        array.splice(parseInt(theAdsD?.num), 1);
+                        await updateDoc(UserRef, {
+                            number: (num - 1),
+                            DataList: array,
+                            Deleted: arrayDeleted
+                        })
+                        await deleteDoc(UserRef1);
+                    }
+                    const AdInUser = await new Promise(async (res, rej) => {
+                        const UserRef = doc(db, "Users", auth?.currentUser?.uid);
+                        const docSnap = await getDoc(UserRef);
+                        const data = docSnap.data();
+                        const NewData = data.MyAds;
+                        const NewData1 = NewData.filter((value) => {
+                            console.log(value.num);
+                            console.log(theAdsD?.num);
+                            return value.num != theAdsD?.num && value.from != theAdsD?.from;
+                        })
+                        await updateDoc(UserRef, {
+                            MyAds: NewData1
+                        })
+                        setIsModalVisible(!isModalVisible);
+                        setLoad2(false);
+                        setValue(value + 1);
+                    })
+                    Promise.all(AdInFire , AdInUser);
+                })
+                
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: (colorScheme == 'dark') ? 'black' : 'white' }}>
@@ -104,15 +166,15 @@ export default function MyAds() {
                     <ReactView style={{ flex: 1 }}>
                         {!load2 ? (
                             <ReactView style={{ height: "100%", width: "100%", alignItems: 'center', justifyContent: 'center', }}>
-                                <ReactView style={{ width: "90%", height: 90, backgroundColor: "white", borderRadius: 10, padding: 10 }}>
-                                    <TouchableOpacity onPress={() => {
+                                <ReactView style={{ width: "90%", backgroundColor: "white", borderRadius: 10, padding: 10 }}>
+                                    <TouchableOpacity style={{ padding: 10 }} onPress={() => {
                                         setLoad2(true);
                                     }}>
                                         <ReactText style={{ fontSize: 15, fontWeight: 'bold' }}>
                                             Delete It
                                         </ReactText>
                                     </TouchableOpacity>
-                                    <TouchableOpacity onPress={() => {
+                                    <TouchableOpacity style={{ padding: 10 }} onPress={() => {
                                         handleModal();
                                     }}>
                                         <ReactText style={{ fontSize: 15, fontWeight: 'bold' }}>
@@ -125,14 +187,15 @@ export default function MyAds() {
                         ) : (
                             <ReactView style={{ height: "100%", width: "100%", alignItems: 'center', justifyContent: 'center', }}>
 
-                                <ReactView style={{ width: "90%", height: 90, backgroundColor: "white", borderRadius: 10, padding: 10 }}>
+                                <ReactView style={{ width: "90%", backgroundColor: "white", borderRadius: 10, padding: 10 }}>
                                     <ReactText style={{ fontSize: 15, fontWeight: 'bold' }}>
                                         Are you sure for delete it?
                                     </ReactText>
                                     <ReactView style={{ flexDirection: direction.direction, alignItems: direction.start, width: "100%" }}>
-                                        <TouchableOpacity style={{ padding: 10 }} onPress={() => {
-                                            setLoad2(false);
+                                        <TouchableOpacity style={{ padding: 10 }} onPress={async () => {
+                                            await DeleteTheAds(deleteIt);
                                             handleModal();
+                                            setLoad2(false);
                                         }}>
                                             <ReactText style={{ fontSize: 15, fontWeight: 'bold' }}>
                                                 Yes
@@ -165,7 +228,8 @@ export default function MyAds() {
                                         </Text>
                                     </ReactView>
                                     <TouchableOpacity style={{ width: "10%", alignItems: 'center' }} onPress={() => {
-                                        // setLoad(true);
+                                        setDeleteIt(value);
+                                        setindexItem(index);
                                         handleModal();
                                     }}>
                                         <ReactView style={{ width: 4, height: 4, borderRadius: 50, backgroundColor: 'black', marginBottom: 2 }} />
